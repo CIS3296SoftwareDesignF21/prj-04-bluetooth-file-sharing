2a14d292282e8abb63c77c79e2cc15372766352a
tuj45886 <tuj45886@temple.edu>
2021-11-09 16:21:58 -0500

    Added Package messages that'll be used to create objects of messages that will be sent over bluetooth

---
+++ app/src/main/java/com/example/app/messages/messages.java
@@ -0,0 +1,16 @@
+package com.example.app.messages;
+
+public class messages {
+    public String text;
+    public String sender;
+    public String target;//receiver
+    public String FileType;
+    int timeRecievd;
+    //message object will be sent over bT impl
+
+
+
+
+
+
+}
