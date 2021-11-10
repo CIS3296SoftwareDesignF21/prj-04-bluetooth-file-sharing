3e9eb493ab1924e253312f325dacb1af31f19f1b
tuj45886 <tuj45886@temple.edu>
2021-11-09 19:43:42 -0500

    Added a constructor to the messages.java, also added methods toString(), equals(), and hashCode(). These methods were mainly Overrides of already existing ones for java objects. In addition to this, I added method arrivedBefore() that compares two messages and returns true if the current message has an earlier arrival time than that of the message passed to it.

--- app/src/main/java/com/example/app/messages/messages.java
+++ app/src/main/java/com/example/app/messages/messages.java
@@ -1,15 +1,63 @@
 package com.example.app.messages;
 
+import android.os.Build;
+import androidx.annotation.RequiresApi;
+
+import java.util.Objects;
+
 public class messages {
-    public String text;
-    public String sender;
-    public String target;//receiver
+
+
+    public String text;//realy just a stand in for the name of anything recieved over bluetooth
+    public String sender;//id of device sending the packet
+    public String target;//id of device sending the packet
     public String FileType;
-    int timeRecievd;
+    public int timeRecievd;
     //message object will be sent over bT impl
 
+    public messages(String text, String sender, String target, int timeRecievd, String FileType){
+        this.text = text;
+        this.sender = sender;
+        this.target = target;
+        this.FileType = FileType;
+        this.timeRecievd = timeRecievd;
+    }
+
+    public boolean arrivedBefore(messages B){//returns true if the Recived Time of the current message is less than the
+        //message passed as a parameter
+        if(this.timeRecievd < B.timeRecievd){
+            return true;
+        }
+        return false;
+    }
 
 
+
+    @Override
+    public String toString() {//edit if any changes to the constructor is made
+        return "messages{" +
+                "text='" + text + '\'' +
+                ", sender='" + sender + '\'' +
+                ", target='" + target + '\'' +
+                ", FileType='" + FileType + '\'' +
+                ", timeRecievd=" + timeRecievd +
+                '}';
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.KITKAT)
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        messages messages = (messages) o;
+        return timeRecievd == messages.timeRecievd && Objects.equals(text, messages.text) && Objects.equals(sender, messages.sender) && Objects.equals(target, messages.target) && Objects.equals(FileType, messages.FileType);
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.KITKAT)
+    @Override
+    public int hashCode() {
+        return Objects.hash(text, sender, target, FileType, timeRecievd);
+    }
 
 
 
