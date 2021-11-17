17abdda2258b60a1d927179a404af4c25f09f8e8
ZackWaxler <tuj00149@temple.edu>
2021-11-16 19:53:58 -0500

    Still working on testing it

--- app/src/main/java/com/example/app/storage/FileStorage.kt
+++ app/src/main/java/com/example/app/storage/FileStorage.kt
@@ -1,6 +1,9 @@
 package com.example.app.storage
 
 import java.io.*
+import android.content.Context
+import android.os.Build
+import androidx.annotation.RequiresApi
 
 
 /* References:
@@ -13,11 +16,53 @@ import java.io.*
 * TODO:
 *  Testing
 *  Update manifest to ensure permissions are correct
-*  Look into `tooByteArray`
-*  Look into accessing the context
 *  Does not currently compile! (still figuring out some Android app intricacies)
 * */
 
+const val STORAGE_PATH: String = "Bridgefy";
+
+@RequiresApi(Build.VERSION_CODES.LOLLIPOP)
+class FileStorage(private val context: Context) { // Stores context as a class variable (Kotlin feature)
+    init {
+        // Do Nothing
+    }
+
+    /* TODO:
+     * Fix 2gb size limit (readBytes)
+     */
+    fun saveFile(fileName: String, fileContent: File) {
+        var myExternalFile: File = File(context.getExternalFilesDir(STORAGE_PATH), fileName);
+
+        try {
+            val fileOutPutStream = FileOutputStream(myExternalFile)
+            fileOutPutStream.write(fileContent.readBytes())
+            fileOutPutStream.close()
+        } catch (e: IOException) {
+            e.printStackTrace()
+        }
+    }
+
+//    fun getFileFromStorage(fileName: String, fileContent: File) {
+//        var myExternalFile: File = File(context.getExternalFilesDir(STORAGE_PATH), fileName)
+//
+//        var fileInputStream: FileInputStream = FileInputStream(myExternalFile)
+//        var inputStreamReader: InputStreamReader = InputStreamReader(fileInputStream)
+//        val bufferedReader: BufferedReader = BufferedReader(inputStreamReader)
+//        val stringBuilder: StringBuilder = StringBuilder()
+//        var text: String? = null
+//
+//        while (run {
+//                text = bufferedReader.readLine()
+//                text
+//            } != null) {
+//            stringBuilder.append(text)
+//        }
+//
+//        fileInputStream.close()
+//    }
+}
+
+/*
 const val STORAGE_PATH: String = "Bridgefy"
 
 fun getFileFromStorage(fileName: String, fileContent: File) {
@@ -51,3 +96,4 @@ fun saveFile(fileName: String, fileContent: File) {
         e.printStackTrace()
     }
 }
+*/
