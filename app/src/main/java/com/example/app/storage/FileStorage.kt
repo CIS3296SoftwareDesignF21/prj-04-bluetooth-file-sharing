8ad18b30bb472941d014d76d0affc90ce7db2625
ZackWaxler <tuj00149@temple.edu>
2021-11-16 21:14:15 -0500

    Finished file save functionality, need to discuss how to implement file uploading

--- app/src/main/java/com/example/app/storage/FileStorage.kt
+++ app/src/main/java/com/example/app/storage/FileStorage.kt
@@ -3,8 +3,8 @@ package com.example.app.storage
 import java.io.*
 import android.content.Context
 import android.os.Build
+import android.os.Environment
 import androidx.annotation.RequiresApi
-
 
 /* References:
 * https://stackoverflow.com/questions/5092591/what-are-the-differences-among-internal-storage-external-storage-sd-card-and-r
@@ -13,87 +13,39 @@ import androidx.annotation.RequiresApi
 */
 
 /*
-* TODO:
-*  Testing
-*  Update manifest to ensure permissions are correct
-*  Does not currently compile! (still figuring out some Android app intricacies)
-* */
-
-const val STORAGE_PATH: String = "Bridgefy";
+ * TODO:
+ *  Discuss file upload strategy (i.e. should we use a file picker, specify a path, etc.)
+ */
 
 @RequiresApi(Build.VERSION_CODES.LOLLIPOP)
 class FileStorage(private val context: Context) { // Stores context as a class variable (Kotlin feature)
-    init {
-        // Do Nothing
-    }
-
-    /* TODO:
-     * Fix 2gb size limit (readBytes)
+    /* saveFile(fileName: String, fileContent: ByteArray)
+     * - Saves the specified byte array to the downloads folder using the file name given
+     * - Should automatically handle duplicate filenames
+     * TODO:
+      * Fix 2gb size limit (readBytes) (we will totally fix this before the project is due)
      */
-    fun saveFile(fileName: String, fileContent: File) {
-        var myExternalFile: File = File(context.getExternalFilesDir(STORAGE_PATH), fileName);
 
+    fun saveFile(fileName: String, fileContent: ByteArray) {
+        val path = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS);
+
+        var myExternalFile: File = File(path, "/test.txt");
         try {
             val fileOutPutStream = FileOutputStream(myExternalFile)
-            fileOutPutStream.write(fileContent.readBytes())
+            fileOutPutStream.write(fileContent)
             fileOutPutStream.close()
         } catch (e: IOException) {
             e.printStackTrace()
         }
-    }
 
-//    fun getFileFromStorage(fileName: String, fileContent: File) {
-//        var myExternalFile: File = File(context.getExternalFilesDir(STORAGE_PATH), fileName)
-//
-//        var fileInputStream: FileInputStream = FileInputStream(myExternalFile)
-//        var inputStreamReader: InputStreamReader = InputStreamReader(fileInputStream)
-//        val bufferedReader: BufferedReader = BufferedReader(inputStreamReader)
-//        val stringBuilder: StringBuilder = StringBuilder()
-//        var text: String? = null
-//
-//        while (run {
-//                text = bufferedReader.readLine()
-//                text
-//            } != null) {
-//            stringBuilder.append(text)
-//        }
-//
-//        fileInputStream.close()
-//    }
-}
+        /* Example Invocation:
 
-/*
-const val STORAGE_PATH: String = "Bridgefy"
+            // Test file storage
+            val fileName = "test.txt"
+            val fileContents = "testing123".toByteArray()
 
-fun getFileFromStorage(fileName: String, fileContent: File) {
-    var myExternalFile:File = File(context.getExternalFilesDir(STORAGE_PATH), fileName)
-
-    myExternalFile = File(context.getExternalFilesDir(STORAGE_PATH),fileName)
-
-    var fileInputStream = FileInputStream(myExternalFile)
-    var inputStreamReader: InputStreamReader = InputStreamReader(fileInputStream)
-    val bufferedReader: BufferedReader = BufferedReader(inputStreamReader)
-    val stringBuilder: StringBuilder = StringBuilder()
-    var text: String? = null
-
-    while (run {
-            text = bufferedReader.readLine()
-            text
-        } != null) {
-        stringBuilder.append(text)
-    }
-
-    fileInputStream.close()
-}
-
-fun saveFile(fileName: String, fileContent: File) {
-    var myExternalFile: File = File(context.getExternalFilesDir(STORAGE_PATH), fileName);
-    try {
-        val fileOutPutStream = FileOutputStream(myExternalFile)
-        fileOutPutStream.write(fileContent.toByteArray())
-        fileOutPutStream.close()
-    } catch (e: IOException) {
-        e.printStackTrace()
+            val fs: FileStorage = FileStorage(requireContext())
+            fs.saveFile(fileName, fileContents)
+         */
     }
 }
-*/
