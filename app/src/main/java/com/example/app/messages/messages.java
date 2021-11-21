0fbadf45b8f9b21526f981be9bbce5847751f338
tuj45886 <tuj45886@temple.edu>
2021-11-21 15:31:16 -0500

    Changed Added variable FilePath to Constructor, created a method to Load the Files into memory. In the messagesTestClass, a created an array of messages. MOst impoirtantly I was able to run the test class using the Jetbrains  Runtime ... 11

--- app/src/main/java/com/example/app/messages/messages.java
+++ app/src/main/java/com/example/app/messages/messages.java
@@ -3,33 +3,32 @@ package com.example.app.messages;
 import android.os.Build;
 import androidx.annotation.RequiresApi;
 
-import java.util.Collection;
-import java.util.Comparator;
+import java.io.File;
 import java.util.Objects;
 
 public class messages {//message object will be sent over bT impl
 
 
-    public String text;//realy just a stand in for the name of anything recieved over bluetooth
+    public String FilePath;//realy just a stand in for the name of anything recieved over bluetooth
     public String sender;//id of device sending the packet
     public String target;//id of device sending the packet
     public String FileType;
-    public int timeRecievd;
+    public int timeReceived;
     public int sizeInMem; //reffering to the messages size in memory
 
 
-    public messages(String text, String sender, String target, int timeRecievd, String FileType, int sizeInMem){
-        this.text = text;
+    public messages(String FilePath, String sender, String target, int timeReceived, String FileType, int sizeInMem){
+        this.FilePath = FilePath;
         this.sender = sender;
         this.target = target;
         this.FileType = FileType;
-        this.timeRecievd = timeRecievd;
+        this.timeReceived = timeReceived;
         this.sizeInMem = sizeInMem;
     }
 
     public boolean arrivedBefore(messages B){//returns true if the Recived Time of the current message is less than the
         //message passed as a parameter
-        if(this.timeRecievd < B.timeRecievd){
+        if(this.timeReceived < B.timeReceived){
             return true;
         }
         return false;
@@ -40,11 +39,11 @@ public class messages {//message object will be sent over bT impl
     @Override
     public String toString() {//edit if any changes to the constructor is made
         return "messages{" +
-                "text='" + text + '\'' +
+                "FilePath='" + FilePath + '\'' +
                 ", sender='" + sender + '\'' +
                 ", target='" + target + '\'' +
                 ", FileType='" + FileType + '\'' +
-                ", timeRecievd=" + timeRecievd +
+                ", timeRecievd=" + timeReceived +
                 '}';
     }
 
@@ -56,20 +55,26 @@ public class messages {//message object will be sent over bT impl
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         messages messages = (messages) o;
-        return timeRecievd == messages.timeRecievd && Objects.equals(text, messages.text) && Objects.equals(sender, messages.sender) && Objects.equals(target, messages.target) && Objects.equals(FileType, messages.FileType);
+        return timeReceived == messages.timeReceived && Objects.equals(FilePath, messages.FilePath) && Objects.equals(sender, messages.sender) && Objects.equals(target, messages.target) && Objects.equals(FileType, messages.FileType);
     }
 
     @RequiresApi(api = Build.VERSION_CODES.KITKAT)
     @Override
     public int hashCode() {
-        return Objects.hash(text, sender, target, FileType, timeRecievd);
+        return Objects.hash(FilePath, sender, target, FileType, timeReceived);
+    }
+
+
+
+    public File loadFile(){
+        File cuurentItem = new File(this.FilePath);
+        return null;
+        
     }
 
 
     //next add a comparator so sorting with arrays is mad easy
     //with CComparator you can compare by different factors
     //comparable sets a default item to compare by and may be required by soring functions of other data types
-
-
 
 }
--- app/src/main/java/com/example/app/messages/messagesTestClass.java
+++ app/src/main/java/com/example/app/messages/messagesTestClass.java
@@ -5,7 +5,7 @@ import java.util.ArrayList;
 
 public class messagesTestClass {
     public static void main(String[] args) {
-         String text = "This is the message";//realy just a stand in for the name of anything recieved over bluetooth
+         String FilePath = "This is the message";//realy just a stand in for the name of anything recieved over bluetooth
          String sender = "Sender ID";//id of device sending the packet
          String target = "Reciever ID";//id of device sending the packet
          String FileType = "pdf";
@@ -13,8 +13,6 @@ public class messagesTestClass {
          int sizeInMem = 5186;//in some standard unit
 
 
-
-        messages TestMessgae = new messages(text, sender, target,timeRecievd, FileType, sizeInMem);
         //make a for loop and create and add a new messgae to the array list each time by modifying each one per iteration
         //test what ArrayList fnctions are able to work
         //in check benefits of making an object comparable and benefits of making implementing Comparator in an object to know what you are missing.
@@ -22,8 +20,17 @@ public class messagesTestClass {
         //Work with Zack to see how he wants the object stored
         //
 
+      messages rep[] =new messages[4];
 
-        ArrayList<messages> works = new ArrayList<>();
+      for(int i = 0; i < rep.length; i++){
+          //messages(String FilePath, String sender, String target, int timeReceived, String FileType, int sizeInMem)
+          rep[i] = new messages(FilePath , sender, target, timeRecievd, FileType, sizeInMem);
+      }
+
+      for(int i = 0; i < rep.length; i++){
+          System.out.println(rep[i]);
+      }
+
 
 
 
